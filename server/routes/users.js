ad71916dd337f009205a8cae8a379860c4b3a045
MarshmallowKA24 <alejandrolopez3131@gmail.com>
2025-07-24 22:27:17 +0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
+// server/routes/users.js
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const auth = require('../middleware/auth');
+const auth = require('../middleware/auth'); // This imports the object containing all auth functions
 
-router.get('/profile', auth, async (req, res) => {
+// Use auth.authenticateToken specifically
+router.get('/profile', auth.authenticateToken, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
@@ -12,7 +14,8 @@ router.get('/profile', auth, async (req, res) => {
   }
 });
 
-router.put('/profile', auth, async (req, res) => {
+// Use auth.authenticateToken specifically here too
+router.put('/profile', auth.authenticateToken, async (req, res) => {
   try {
     const user = await User.findByIdAndUpdate(req.user.id, req.body, { new: true }).select('-password');
     res.json(user);
